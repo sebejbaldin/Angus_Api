d066887750a5d96bcfa175e243bddf29e845eece
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-04 20:23:13 +0200

    Added many comment

--- database/dbcon.js
+++ database/dbcon.js
@@ -16,7 +16,8 @@ const influx = new Influx.InfluxDB({
 });
 
 var conn = mysql.createConnection(configMSSQL);
-
+// i'll comment only the first function of this type because the concept is the same for all
+// for the description of the functions called inside this scroll down
 exports.getGlobalEnergySumLM = async () => {
     let MyQuery = `select m.name, s.id, s.type from machines m 
     join sensors s on s.machine_id=m.id 
@@ -25,8 +26,15 @@ exports.getGlobalEnergySumLM = async () => {
     from (select * from testdata group by tag_sensor_id )
     where time > now() - 1m group by tag_sensor_id`;
 
+    // after have prepared the query you call the GetData function in which you pass three arguments
+    // first mysql query, second a influx query, third a function to elaborate the data
     return await GetData(MyQuery, InQuery, async (MyRes, InRes) => {
-        // the data are being processed after have being received from the database 
+        // what is about to happen is:
+        // 1) use the function ReadQueryMySQL using the query passed
+        // 2) same but using the influx function ad influx query
+        // 3) pass to the function providedthe results of the queryes and return the data 
+                
+        // this is what will be do to the data in this case
         let obj = {
             descr: 'globalEnergySumLastMinute'
         };
@@ -73,10 +81,6 @@ exports.getEnergyLastMinuteBySens = async () => {
     });
 };
 
-/*
-query consumo istantaneo per ogni sensor id:
-  select * from testdata group by tag_sensor_id order by time desc limit 1
-*/
 exports.getInstantEnergyDrainForSensor = async () => {
     let MyQuery = `select * from sensors`;
     let InQuery = `select * from testdata group by tag_sensor_id order by time desc limit 1`;
@@ -158,7 +162,7 @@ exports.insertMeasurement = (res, data) => {
 // this function executes all the query on the mysql database
 async function ReadQueryMySQL(query) {
     if (queryIsValid(query)) {
-        // inizialized the container of the result
+        // inizialize the container of the result
         let mysqlRes = [];
         // connect to the database
         conn.connect();
@@ -167,7 +171,9 @@ async function ReadQueryMySQL(query) {
             conn.end();
             // after have received response the connection is being closed        
             console.log(resultMy);
-            // the data are being inserted
+            // the data are being inserted in the container
+            // for some reasons i can't simply assign or return directly the result
+            // if i try to do so out of the scope of the callback function the data vanish
             resultMy.forEach(x => {
                 mysqlRes.push(x);
             });
@@ -176,7 +182,7 @@ async function ReadQueryMySQL(query) {
                 throw err;
             }
             // if i only assign the result to the external variable, the data doesn't get out of the function
-            //mysqlRes = resultMy;
+            // mysqlRes = resultMy;
         });
         return mysqlRes;
     } else
@@ -203,11 +209,16 @@ async function ReadQueryInfluxDB(query) {
         return {};
 };
 
+// this function is the one to use to get data from the database
+// it's parameters are (in the same order): the query for mysql db and the one for influx db, the function to process the data to return
+// i have split the try/catch in three because as is i can send personalized error to source
+// i don't like much this approach but since you actually can't catch specific exception with
+// differenct catch branches i don't know how to do it better
 async function GetData(qMySQL, qInflux, processor) {
     let dataMy = {};
     let dataIn = {};
     try {
-
+        //the first query is executed on the mysql db
         dataMy = await ReadQueryMySQL(qMySQL);
 
     } catch (errz) {
@@ -217,7 +228,7 @@ async function GetData(qMySQL, qInflux, processor) {
         };
     }
     try {
-
+        // second query is executed on the influx db 
         dataIn = await ReadQueryInfluxDB(qInflux);
 
     } catch (errz) {
@@ -227,17 +238,20 @@ async function GetData(qMySQL, qInflux, processor) {
         };
     }
     try {
-
+        // the data is passed to the function provided, processed and returned
         return await processor(dataMy, dataIn);
 
     } catch (errz) {
         console.log(errz);
         return {
-            err: 'Error on processing data'
+            err: 'Error on processing data',
+            mysql_data: dataMy,
+            influx_data: dataIn
         };
     }
 }
 
+// this function do some check to the query, it should be extended later
 function queryIsValid(query) {
     try {
         let string = "";
