20b71a009b9bbbe720b6a29089e9f42f91a3574e
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-16 12:51:26 +0200

    Changed few configurations

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
+// 10.10.10.1:3306 - piedpiper:PiedPiper2018
 module.exports = {
-  jwt_secret: "A7BB5A1CEB2D9D5174C3D0DF08E70CD7320CA86F2AFB6CF05A287BD4E9DD27E0",
+  jwt_secret: 'A7BB5A1CEB2D9D5174C3D0DF08E70CD7320CA86F2AFB6CF05A287BD4E9DD27E0',
   mysql_config: {
-    hostname: "localhost",
+    hostname: '10.10.10.1',
     port: 3306,
-    username: "piedpiper",
-    password: "piedpiper",
-    database: "db_piedpiper"
+    username: 'root',
+    password: 'Passw0rd!',
+    database: 'db_piedpiper'
   },
   influx_config: {
-    hostname: '192.168.101.81',
+    hostname: '10.10.10.6',
     port: 8086,
     database: 'Angus_v1'
   }
