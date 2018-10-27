c1f21a9112188d1a829afbd6400ab7e4df32b6df
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-27 15:28:23 +0200

    Change mysql config to use virtualized server on local machine

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 module.exports = {
   jwt_secret: 'A7BB5A1CEB2D9D5174C3D0DF08E70CD7320CA86F2AFB6CF05A287BD4E9DD27E0',
   mysql_config: {
-    hostname: '10.10.10.1',
+    hostname: '192.168.146.40',
     port: 3306,
-    username: 'root',
-    password: 'Passw0rd!',
+    username: 'piedpiper',
+    password: 'password',
     database: 'db_piedpiper'
   },
   influx_config: {
-    hostname: '10.10.10.6',
+    hostname: '192.168.101.42',
     port: 8086,
     database: 'Angus_v1'
   }
